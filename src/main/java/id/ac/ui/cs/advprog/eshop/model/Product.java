e5197d4cc303973d1bf53463865136e39daf8c73
Valentino <valentino.kim@ui.ac.id>
2025-02-12 05:38:54 +0700

    feat: added product repository

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -1,4 +1,4 @@
-package id.ac.ui.cs.hadvprog.eshop.model;
+package id.ac.ui.cs.advprog.eshop.model;
 
 import lombok.Getter;
 import lombok.Setter;
--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -1,4 +1,4 @@
-package id.ac.ui.cs.hadvprog.eshop;
+package id.ac.ui.cs.advprog.eshop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
