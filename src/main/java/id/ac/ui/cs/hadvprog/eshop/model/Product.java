18ba39f49eff93fd5b62ff8c59a264afaff0fdf2
Valentino <valentino.kim@ui.ac.id>
2025-02-12 05:34:07 +0700

    feat: added product model

---
+++ src/main/java/id/ac/ui/cs/hadvprog/eshop/model/Product.java
@@ -0,0 +1,11 @@
+package id.ac.ui.cs.hadvprog.eshop.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Product {
+    private String productId;
+    private String productName;
+    private int productQuantity;
+}
