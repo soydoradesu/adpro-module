aa8bd5a466d46ded3accccddad119cf805dc3a0e
Valentino <valentino.kim@ui.ac.id>
2025-02-12 20:44:38 +0700

    feat: added homepage

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomepageController.java
@@ -0,0 +1,14 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomepageController {
+    @GetMapping(("/"))
+    public String Homepage() {
+        return "Homepage";
+    }
+}
