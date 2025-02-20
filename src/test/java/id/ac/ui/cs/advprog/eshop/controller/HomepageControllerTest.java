af84c5c7dc76d03e15bc63bb4dce3db184c2c2f8
Valentino <valentino.kim@ui.ac.id>
2025-02-20 20:39:15 +0700

    test: added product controller test

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/HomepageControllerTest.java
@@ -0,0 +1,25 @@
+// File: HomepageControllerTest.java
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HomepageControllerTest {
+    @InjectMocks
+    private HomepageController homepageController;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testHomepage() {
+        String viewName = homepageController.Homepage();
+        assertEquals("Homepage", viewName);
+    }
+}
