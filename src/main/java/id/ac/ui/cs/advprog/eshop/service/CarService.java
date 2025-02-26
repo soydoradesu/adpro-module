9459f7b8f745f594784107e7e3169de400c3070f
Valentino <valentino.kim@ui.ac.id>
2025-02-26 23:29:24 +0700

    feat: added Car Controller

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/CarService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/CarService.java
@@ -8,5 +8,5 @@ public interface CarService {
     List<Car> findAll();
     Car findById(String carId);
     void update(String carId, Car car);
-    void deleteCarByIf(String carId);
+    void deleteCarById(String carId);
 }
