23cb19650b313b0cf3d93a519393a24595614da9
Valentino <valentino.kim@ui.ac.id>
2025-02-27 10:51:54 +0700

    refactor: Implementing SRP (Product model now doesn't do randomization of UUID, it's repository's work now

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -13,6 +13,9 @@ public class ProductRepository {
     private List<Product> productData = new ArrayList<>();
 
     public Product create(Product product) {
+        if (product.getProductId() == null) {
+            product.setProductId(UUID.randomUUID());
+        }
         productData.add(product);
         return product;
     }
