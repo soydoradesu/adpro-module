09f40ed758a9271be2a55e6d2840cd4390a41504
Valentino <valentino.kim@ui.ac.id>
2025-03-07 15:49:34 +0700

    [REFACTOR] Fix Typo on enum

--- src/main/java/id/ac/ui/cs/advprog/eshop/enums/PaymentStatus.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/enums/PaymentStatus.java
@@ -15,7 +15,7 @@ public enum PaymentStatus {
         this.value = paymentStatus;
     }
 
-    public static  boolean contains(String param){
+    public static boolean contains(String param){
         for (PaymentStatus paymentStatus : PaymentStatus.values()){
             if (paymentStatus.name().equals(param)){
                 return true;
